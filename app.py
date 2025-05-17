5ff8152abda27c85dc326e053c05c07f72125463
ushaswi <ushaswireddy3@gmail.com>
2025-05-17 12:26:11 +0530

    Final working version for railway deployment

--- app.py
+++ app.py
@@ -277,4 +277,6 @@ def logout():
 if __name__ == '__main__':
     if not os.path.exists('users.db'):
         init_db()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
